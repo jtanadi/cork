a189915c744c987a1b8f6d73cbb0f50fd70e427e
Jesen Tanadi <mail@jesentanadi.com>
2019-09-16 21:08:05 -0400

    turn off logging

--- backend/data/connection.js
+++ backend/data/connection.js
@@ -1,2 +1,2 @@
 const Sequelize = require("sequelize")
-module.exports = new Sequelize(process.env.DATABASE_URL || "postgres://localhost/cork_db")
+module.exports = new Sequelize(process.env.DATABASE_URL || "postgres://localhost/cork_db", { logging: false })
