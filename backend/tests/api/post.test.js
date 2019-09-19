4bb8d7018f4a4c846779585f52ed423ca53de36a
Jesen Tanadi <mail@jesentanadi.com>
2019-09-19 10:15:04 -0400

    separate api tests and move to an api directory

--- backend/tests/api/post.test.js
+++ backend/tests/api/post.test.js
@@ -1,23 +1,12 @@
 const { expect } = require("chai");
 const supertest = require("supertest");
-const app = supertest(require("../src/app"));
+const app = supertest(require("../../src/app"));
 
-const syncAndSeed = require("../src/data/syncAndSeed");
+const syncAndSeed = require("../../src/data/syncAndSeed");
 
 let obj;
 before(async () => {
   obj = await syncAndSeed();
-});
-
-describe("/api/users endpoint", () => {
-  it("has GET endpoint", () => {
-    return app
-      .get("/api/users")
-      .expect(200)
-      .then(response => {
-        expect(response.body.length).to.not.equal(0);
-      });
-  });
 });
 
 describe("/api/users/:id/posts endpoint", () => {
