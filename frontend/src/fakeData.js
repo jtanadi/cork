b2b898517a13b28cfbcd57f2e5eaa1fc40c3279a
Jesen Tanadi <mail@jesentanadi.com>
2019-10-02 22:52:16 -0400

    add fake data so we don't have to make server calls

---
+++ frontend/src/fakeData.js
@@ -0,0 +1,11 @@
+// fake data for now
+const data = [
+  { id: 1, title: "First post!", body: "First text" },
+  { id: 2, title: "Second post!", body: "Second text" },
+  { id: 3, title: "Third post!", body: "Third text" },
+  { id: 4, title: "Fourth post!", body: "Fourth text" },
+  { id: 5, title: "Fifth post!", body: "Fifth text" },
+  { id: 6, title: "Sixth post!", body: "Sixth text" }
+];
+
+export default data;
